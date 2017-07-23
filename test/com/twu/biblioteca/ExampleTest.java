299a84f69a24193c48df1e921547b396beb42a21
Responsible <responsible01@yeah.net>
2017-07-23 23:44:37 +0800

    add welcome message

--- test/com/twu/biblioteca/ExampleTest.java
+++ test/com/twu/biblioteca/ExampleTest.java
@@ -1,13 +1,35 @@
 package com.twu.biblioteca;
 
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 import static org.junit.Assert.assertEquals;
 
 public class ExampleTest {
+    private final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+    private final ByteArrayOutputStream errStream = new ByteArrayOutputStream();
+
+    @Before
+    public void setOutStream() {
+        System.setOut(new PrintStream(outputStream));
+        System.setErr(new PrintStream(errStream));
+    }
+
+    @After
+    public void resetOutStream() {
+        System.setOut(null);
+        System.setErr(null);
+    }
 
     @Test
-    public void test() {
-        assertEquals(1, 1);
+    public void testWelcome() {
+        BibliotecaApp bibliotecaApp = new BibliotecaApp();
+        bibliotecaApp.main(null);
+        assertEquals("Welcome to Biblioteca Public Library!",outputStream.toString());
     }
 }
