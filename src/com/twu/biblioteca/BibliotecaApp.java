8fa4f1d45299699c2e84c70df119dbfdf0488f40
Responsible <responsible01@yeah.net>
2017-07-24 17:51:09 +0800

    add main menu

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,5 +1,7 @@
 package com.twu.biblioteca;
 
+import java.util.Scanner;
+
 public class BibliotecaApp {
     private Book[] books = {new Book("Book1", "Author1", 2016),
             new Book("Book2", "Author2", 2016),
@@ -8,7 +10,7 @@ public class BibliotecaApp {
     public static void main(String[] args) {
         BibliotecaApp bibliotecaApp = new BibliotecaApp();
         bibliotecaApp.printWelcome();
-        bibliotecaApp.printBookDetails();
+        bibliotecaApp.printMainMenu();
     }
 
     public void printWelcome() {
@@ -27,6 +29,27 @@ public class BibliotecaApp {
         System.out.println(BOOK_COLUMN);
         for (Book book : books) {
             System.out.printf("%s\t%s\t%s\n", book.getName(), book.getAuthor(), book.getYear());
+        }
+    }
+
+    public void printMainMenu() {
+        String MAIN_MENU_HEAD = "---------------\n" + "" +
+                "Menu\n"
+                + "---------------";
+        String[] MAIN_MENU_ITEM = {"1. List Books"};
+        String MAIN_MENU_TIP = "---------------\nPlease select your choice (1-1):";
+        System.out.println(MAIN_MENU_HEAD);
+        for (String item : MAIN_MENU_ITEM) {
+            System.out.println(item);
+        }
+        System.out.println(MAIN_MENU_TIP);
+
+        Scanner input = new Scanner(System.in);
+        Integer userChoice = input.nextInt();
+        switch (userChoice) {
+            case 1:
+                this.listBook();
+                break;
         }
     }
 }
--- test/com/twu/biblioteca/ExampleTest.java
+++ test/com/twu/biblioteca/ExampleTest.java
@@ -5,14 +5,13 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
+import java.io.*;
 
 import static org.junit.Assert.assertEquals;
 
 public class ExampleTest {
-    private final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-    private final ByteArrayOutputStream errStream = new ByteArrayOutputStream();
+    private final OutputStream outputStream = new ByteArrayOutputStream();
+    private final OutputStream errStream = new ByteArrayOutputStream();
     private BibliotecaApp bibliotecaApp = new BibliotecaApp();
     private final String WELCOME_MSG = "Welcome to Biblioteca Public Library!\n";
     private final String[] BOOKS_NAME = {"Book1", "Book2", "Book3"};
@@ -20,6 +19,11 @@ public class ExampleTest {
     private final Integer[] BOOKS_YEAR = {2016, 2016, 2017};
     private final String BOOK_COLUMN = "Title\tAuthor\tYear\n" +
             "--------------------\n";
+    private final String MAIN_MENU_HEAD = "---------------\n" +
+            "Menu\n"
+            + "---------------\n";
+    private final String[] MAIN_MENU_ITEM = {"1. List Books"};
+    private final String MAIN_MENU_TIP = "\n---------------\nPlease select your choice (1-1):";
 
     @Before
     public void setOutStream() {
@@ -35,12 +39,12 @@ public class ExampleTest {
 
     @Test
     public void testMain() {
+        System.setIn(new ByteArrayInputStream("1".getBytes()));
         BibliotecaApp.main(null);
-        assertEquals(WELCOME_MSG +
-                BOOK_COLUMN +
-                String.format("%s\t%s\t%s\n", BOOKS_NAME[0], BOOKS_AUTHOR[0], BOOKS_YEAR[0]) +
-                String.format("%s\t%s\t%s\n", BOOKS_NAME[1], BOOKS_AUTHOR[1], BOOKS_YEAR[1]) +
-                String.format("%s\t%s\t%s\n", BOOKS_NAME[2], BOOKS_AUTHOR[2], BOOKS_YEAR[2]), outputStream.toString());
+        assertEquals(WELCOME_MSG + MAIN_MENU_HEAD +
+                MAIN_MENU_ITEM[0] +
+                MAIN_MENU_TIP + "\n" +
+                String.join("\n", BOOKS_NAME), outputStream.toString().trim());
     }
 
     @Test
@@ -63,5 +67,15 @@ public class ExampleTest {
                         String.format("%s\t%s\t%s\n", BOOKS_NAME[1], BOOKS_AUTHOR[1], BOOKS_YEAR[1]) +
                         String.format("%s\t%s\t%s\n", BOOKS_NAME[2], BOOKS_AUTHOR[2], BOOKS_YEAR[2])
                 , outputStream.toString());
+    }
+
+    @Test
+    public void testMainMenu() {
+        System.setIn(new ByteArrayInputStream("1".getBytes()));
+        bibliotecaApp.printMainMenu();
+        assertEquals(MAIN_MENU_HEAD +
+                MAIN_MENU_ITEM[0] +
+                MAIN_MENU_TIP + "\n" +
+                String.join("\n", BOOKS_NAME), outputStream.toString().trim());
     }
 }
